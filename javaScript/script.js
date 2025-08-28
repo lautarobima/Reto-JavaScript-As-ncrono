d3a40e06f1b2a3fc5799a09e1605c8633f5bb854
Bedoya1905 <santiagobedgo@unisabana.edu.co>
2025-08-28 17:23:50 -0500

    Agregado funcion getCoctail()

---
+++ javaScript/script.js
@@ -0,0 +1,19 @@
+// (todo) Conseguir API de los cocteles
+
+// Coctel (aleatorio)
+const getCoctail = async () => {
+    const url = "www.thecocktaildb.com/api/json/v1/1/random.php";
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP Error: ${response.status}`);
+        }
+
+        const coctailData = await response.json();
+        console.log(coctailData);
+    } catch (error) {
+        console.error(`Error in fetching data: ${error}`);
+    }
+}
+getCoctail();
