14b502c7832a9a739a6813eb0d32ec42db74cbc9
Bedoya1905 <santiagobedgo@unisabana.edu.co>
2025-08-28 18:31:05 -0500

    Agregado presentCocktail(), version primitiva de agregar la informacion del coctel en algun HTML

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,4 +1,4 @@
-// (todo) Conseguir API de los cocteles
+// Conseguir API de los cocteles
 
 // Coctel (aleatorio)
 const getCocktail = async () => {
@@ -23,19 +23,19 @@ getCocktail();
 const getCocktailData = (cocktailData) => {
     console.log(cocktailData["drinks"][0]);
     // Va a estar en español porque no sale las instrucciones en algunos cocteles en español
+    // CUIDADO QUE EN OCACIONES strDrink devuelve 410 Gone, es decir que el coctel ya no existe, muy raro, pero me paso
     const { idDrink, strAlcoholic, strCategory, strDrink, strDrinkThumb, strInstructions } = cocktailData["drinks"][0];
     console.log(idDrink, strAlcoholic, strCategory, strDrink, strDrinkThumb, strInstructions);
 
     const ingredientsArray = getIngredients(cocktailData["drinks"][0]);
-    console.log(ingredientsArray);
+
+    presentCocktail(cocktailData, ingredientsArray);
 };
 
 const getIngredients = (cocktailData) => {
     const ingredientsArray = Object.keys(cocktailData)
         .filter(key => key.startsWith("strIngredient"))
         .map(key => cocktailData[key]);
-
-    console.log(ingredientsArray);
 
     // Esto es un crimen de guerra pero los breaks funcionan raro en JS entonces toco asi
     let searchNull = true;
@@ -49,4 +49,19 @@ const getIngredients = (cocktailData) => {
     }
 
     return ingredientsArray.splice(0, i);
+};
+
+// Mostrar la informacion en una presentacion bonita (cambiar despues con sus respectivos elementos de documento)
+const presentCocktail = (cocktailData, cocktailIngredientsArray) => {
+    // Se que se ve muy raro hacer la exactama misma cosa aqui, pero uno nunca sabe, tal vez lo necesitemos en un futuro
+    const { idDrink, strAlcoholic, strCategory, strDrink, strDrinkThumb, strInstructions } = cocktailData["drinks"][0];
+
+    // Esto se cambia despues con alguna mejor presentacion o algo asi
+    console.log(`ID: ${idDrink}`);
+    console.log(`Alcohol: ${strAlcoholic}`);
+    console.log(`Categoria: ${strCategory}`);
+    console.log(`Nombre: ${strDrink}`);
+    console.log(`Imagen: ${strDrinkThumb}`);
+    console.log(`Ingredientes: ${cocktailIngredientsArray.toString}`);
+    console.log(`Instrucciones:\n\t${strInstructions}`);
 };
