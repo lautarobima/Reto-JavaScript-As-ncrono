686d2810f226160465b62e10a939ccc2fb155a8f
Bedoya1905 <santiagobedgo@unisabana.edu.co>
2025-08-28 17:47:44 -0500

    getCocktailData ahora consigue la id, categoria, nombre, imagen e instrucciones (en ingles)

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,7 +1,6 @@
 // (todo) Conseguir API de los cocteles
 
 // Coctel (aleatorio)
-
 const getCocktail = async () => {
     const url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 
@@ -13,9 +12,17 @@ const getCocktail = async () => {
 
         const coctailData = await response.json();
         console.log(coctailData);
+        getCocktailData(coctailData);
     } catch (error) {
         console.error(`Error in fetching data: ${error}`);
     }
 }
 getCocktail();
 
+// Conseguir data del coctel
+const getCocktailData = (cocktailData) => {
+    console.log(cocktailData["drinks"][0]);
+    // Va a estar en español porque no sale las instrucciones en algunos cocteles en español
+    const { idDrink, strAlcoholic, strCategory, strDrink, strDrinkThumb, strInstructions } = cocktailData["drinks"][0];
+    console.log(idDrink, strAlcoholic, strCategory, strDrink, strDrinkThumb, strInstructions);
+};
